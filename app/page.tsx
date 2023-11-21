b6ef165b0ed3a488aa1007aae9c3abd5b7608c6e
reza <rezamis99vip@gmail.com>
2023-11-21 16:54:07 +0330

    change size for show in mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import LandingDesktop from './component/Desktop/LandingDesktop';
 import { useMediaQuery } from 'react-responsive'
 import LandingMobile from './component/LandingMobile/LandingMobile';
 export default function Home() {
-  const isMobile = useMediaQuery({ query: '(max-width: 425px)' })
+  const isMobile = useMediaQuery({ query: '(max-width: 426px)' })
   return (
     <>
       {isMobile ? (
